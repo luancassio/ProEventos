12a187860ea61ca82ea975142a94defbfb349828
Cassio Silva De Oliveira <lcassio@indracompany.com>
2022-03-28 22:10:32 -0300

    criando token service com JWT

---
+++ Back/ProEventos.API/ProEventos.Application/Dto/TokenService.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using ProEventos.Application.Interface;
+using ProEventos.Domain.Identity;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProEventos.Application.Dto {
+    public class TokenService : ITokenService {
+
+        private readonly IConfiguration _configuration;
+        private readonly UserManager<User> _userManager;
+        private readonly IMapper _mapper;
+
+        public readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration configuration, UserManager<User> userManager, IMapper mapper) {
+            _configuration = configuration;
+            _userManager = userManager;
+            _mapper = mapper;
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["TokenKey"]));
+
+
+        }
+
+        public async Task<string> CreateToken(UserUpdateDto userUptadeDto) {
+
+            var user = _mapper.Map<User>(userUptadeDto);
+
+            var claims = new List<Claim> { // claims criada bseada no usuario
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.UserName)
+            };
+            var roles = await _userManager.GetRolesAsync(user);
+            claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+
+            // criando toekn baseados nas claims, no tempo de exiração, chave de crypto e decryp 
+            var tokenDescription = new SecurityTokenDescriptor {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            //criando token jwt 
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescription);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
